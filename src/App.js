5de77cb6b6dc6b95c712b8c6a4ddfce918e3ab80
Kristian <70901103+Kris-github0@users.noreply.github.com>
2022-12-19 19:57:21 +0000

    Add display component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "./Header.js";
+import Header from "./components/Header";
+import PasswordGenerator from "./components/PasswordGenerator";
 
 function App() {
   return (
     <>
       <Header />
+      <PasswordGenerator />
     </>
   );
 }
