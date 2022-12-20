d87de5145e06dfc2b37e8a61a5e1fad3cc4cbb1d
Kristian <70901103+Kris-github0@users.noreply.github.com>
2022-12-20 13:55:14 +0000

    Add strength component

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Display from "./Display";
+import Strength from "./Strength";
 
 function PasswordGenerator() {
   return (
     <main>
       <Display password={""} copy={() => {}} generatePassword={() => {}} />
+      <Strength password={""} />
     </main>
   );
 }
---
+++ src/components/Strength.js
@@ -0,0 +1,35 @@
+import React from "react";
+import zxcvbn from "zxcvbn";
+
+function Strength({ password }) {
+  let score;
+
+  if (password) {
+    score = zxcvbn(password).score;
+  }
+
+  const strength = {
+    0: "Very weak",
+    1: "Weak",
+    2: "Moderate",
+    3: "Strong",
+    4: "Very strong",
+  };
+
+  return (
+    <section className="strength-container">
+      <h2 className="strength-header">
+        Strength:
+        <span className="strength-text">
+          {" "}
+          {password ? strength[score] : ""}
+        </span>
+      </h2>
+      <div className="meter-container">
+        <div className="meter" strength={password ? score : ""}></div>
+      </div>
+    </section>
+  );
+}
+
+export default Strength;
