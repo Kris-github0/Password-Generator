2b0741875b85fcb8af2e16e79ffafd7792c59845
Kristian <70901103+Kris-github0@users.noreply.github.com>
2022-12-22 21:07:59 +0000

    Add configuration component

--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -8,6 +8,53 @@ function Configuration({ settings, updateSettings, copy }) {
       <div className="configuration-options-container">
         <form onSubmit={(e) => e.preventDefault()}>
           <Length length={50} updateSettings={updateSettings} />
+          <div className="fieldset-container">
+            <fieldset className="include-container">
+              <legend>Include</legend>
+              <label>
+                <input type="checkbox" name="lowercase" />
+                Lowercase
+              </label>
+              <label>
+                <input type="checkbox" name="uppercase" />
+                Uppercase
+              </label>
+              <label>
+                <input type="checkbox" name="numbers" />
+                Numbers
+              </label>
+              <label>
+                <input type="checkbox" name="symbols" />
+                Symbols
+              </label>
+            </fieldset>
+            <fieldset className="exclude-container">
+              <legend>Exclude</legend>
+              <label>
+                <input type="checkbox" name="duplicateCharacters" />
+                Duplicate characters
+              </label>
+              <label>
+                <input type="checkbox" name="similarCharacters" />
+                Similar characters
+              </label>
+              <label>
+                <input type="checkbox" name="successiveCharacters" />
+                Successive characters
+              </label>
+            </fieldset>
+            <fieldset className="alternate-options-container">
+              <legend>Options</legend>
+              <label>
+                <input type="checkbox" name="autoCopy" />
+                Auto-copy
+              </label>
+              <label>
+                <input type="checkbox" name="saveSettings" />
+                Save settings
+              </label>
+            </fieldset>
+          </div>
         </form>
       </div>
     </section>
