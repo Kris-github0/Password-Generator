6070d641ebdfb97c2be40ea5f65f9c3bf8ba2bb9
Kristian <70901103+Kris-github0@users.noreply.github.com>
2022-12-28 20:25:40 +0000

    Setup workbox

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./style.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
