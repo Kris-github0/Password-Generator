51998411f821f3801beb9823269c7b6c541d7435
Kristian <70901103+Kris-github0@users.noreply.github.com>
2023-01-04 15:08:49 +0000

    Organise FAQ component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header.js";
 import PasswordGenerator from "./components/PasswordGenerator/PasswordGenerator.js";
-import FAQ from "./components/FAQ.js";
+import FAQ from "./components/FAQ/FAQ.js";
 
 function App() {
   return (
